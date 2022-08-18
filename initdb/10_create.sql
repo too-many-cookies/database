984334b636522116d41b1635e44174e1b5f87e84
Ethan Ruszanowski <ethan@rusz.dev>
2022-08-18 13:21:53 -0400

    Update init entity names and format

--- initdb/10_create.sql
+++ initdb/10_create.sql
@@ -1,152 +1,121 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
         'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema operanlys
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema operanlys
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `operanlys` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `operanlys`;
+CREATE SCHEMA IF NOT EXISTS operational_analytics DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE operational_analytics;
 
--- -----------------------------------------------------
--- Table `operanlys`.`classinfo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`classinfo`
+
+CREATE TABLE IF NOT EXISTS operational_analytics.class_info
 (
-    `classID`            INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `name`               VARCHAR(255) NULL DEFAULT NULL,
-    `classCode`          VARCHAR(10)  NULL DEFAULT NULL,
-    `classSectionNumber` INT          NULL DEFAULT NULL,
-    PRIMARY KEY (`classID`)
+    `class_id`             INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `name`                 VARCHAR(255) NULL DEFAULT NULL,
+    `class_code`           VARCHAR(10)  NULL DEFAULT NULL,
+    `class_section_number` INT          NULL DEFAULT NULL,
+    PRIMARY KEY (class_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`logs`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`logs`
+CREATE TABLE IF NOT EXISTS operational_analytics.`logs`
 (
-    `logID`     INT          NOT NULL,
-    `logtype`   CHAR(1)      NULL DEFAULT NULL,
+    `log_id`    INT          NOT NULL,
+    `log_type`  CHAR(1)      NULL DEFAULT NULL,
     `username`  VARCHAR(255) NULL DEFAULT NULL,
     `timestamp` DATE         NULL DEFAULT NULL,
-    PRIMARY KEY (`logID`)
+    PRIMARY KEY (log_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`professorinfo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`professorinfo`
+CREATE TABLE IF NOT EXISTS operational_analytics.professor_info
 (
-    `professorID` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `name`        VARCHAR(255) NULL DEFAULT NULL,
-    `username`    VARCHAR(255) NULL DEFAULT NULL,
-    `password`    VARCHAR(255) NULL DEFAULT NULL,
-    PRIMARY KEY (`professorID`)
+    `professor_id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `name`         VARCHAR(255) NULL DEFAULT NULL,
+    `username`     VARCHAR(255) NULL DEFAULT NULL,
+    `password`     VARCHAR(255) NULL DEFAULT NULL,
+    PRIMARY KEY (professor_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`semesterinfo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`semesterinfo`
+CREATE TABLE IF NOT EXISTS operational_analytics.semester_info
 (
-    `semesterID`   INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `semesterCode` INT          NULL DEFAULT NULL COMMENT 'RIT\'s semester code',
-    `name`         VARCHAR(10)  NULL DEFAULT NULL,
-    PRIMARY KEY (`semesterID`)
+    `semester_id`   INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `semester_code` INT          NULL DEFAULT NULL COMMENT 'RIT\'s semester code',
+    `name`          VARCHAR(10)  NULL DEFAULT NULL,
+    PRIMARY KEY (semester_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`signinlogs`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`signinlogs`
+CREATE TABLE IF NOT EXISTS operational_analytics.sign_in_logs
 (
-    `logID`               INT             NOT NULL,
-    `signInLogID`         INT             NOT NULL,
-    `attemptedPassword`   VARCHAR(255)    NULL DEFAULT NULL,
-    `successfulLogIn`     ENUM ('Y', 'N') NULL DEFAULT NULL,
-    `matchExistingRecord` ENUM ('Y', 'N') NULL DEFAULT 'N',
-    PRIMARY KEY (`signInLogID`, `logID`),
-    INDEX `fk_signInLogs_logs_idx` (`logID` ASC) VISIBLE,
-    CONSTRAINT `fk_signInLogs_logs`
-        FOREIGN KEY (`logID`)
-            REFERENCES `operanlys`.`logs` (`logID`)
+    `log_id`                INT             NOT NULL,
+    `sign_in_log_id`        INT             NOT NULL,
+    `attempted_password`    VARCHAR(255)    NULL DEFAULT NULL,
+    `successful_log_in`     ENUM ('Y', 'N') NULL DEFAULT NULL,
+    `match_existing_record` ENUM ('Y', 'N') NULL DEFAULT 'N',
+    PRIMARY KEY (sign_in_log_id, log_id),
+    INDEX `sign_in_logs_idx` (log_id ASC) VISIBLE,
+    CONSTRAINT sign_in_logs_logs_fk
+        FOREIGN KEY (log_id)
+            REFERENCES operational_analytics.`logs` (log_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`studentinfo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`studentinfo`
+CREATE TABLE IF NOT EXISTS operational_analytics.student_info
 (
-    `studentID`       INT UNSIGNED    NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `name`            VARCHAR(255)    NULL DEFAULT NULL,
-    `username`        VARCHAR(255)    NULL DEFAULT NULL,
-    `password`        VARCHAR(255)    NULL DEFAULT NULL,
-    `passwordChanged` ENUM ('Y', 'N') NULL DEFAULT NULL,
-    `lastSignIn`      DATE            NULL DEFAULT NULL,
-    PRIMARY KEY (`studentID`)
+    `student_id`       INT UNSIGNED    NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `name`             VARCHAR(255)    NULL DEFAULT NULL,
+    `username`         VARCHAR(255)    NULL DEFAULT NULL,
+    `password`         VARCHAR(255)    NULL DEFAULT NULL,
+    `password_changed` ENUM ('Y', 'N') NULL DEFAULT NULL,
+    `last_sign_in`     DATE            NULL DEFAULT NULL,
+    PRIMARY KEY (student_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`studentclassinstance`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`studentclassinstance`
+CREATE TABLE IF NOT EXISTS operational_analytics.student_class_instance
 (
-    `instanceID`  INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `studentID`   INT UNSIGNED NULL DEFAULT NULL,
-    `professorID` INT UNSIGNED NULL DEFAULT NULL,
-    `classID`     INT UNSIGNED NULL DEFAULT NULL,
-    `semesterID`  INT UNSIGNED NULL DEFAULT NULL,
-    PRIMARY KEY (`instanceID`),
-    INDEX `studentID` (`studentID` ASC) VISIBLE,
-    INDEX `professorID` (`professorID` ASC) VISIBLE,
-    INDEX `classID` (`classID` ASC) VISIBLE,
-    INDEX `semesterID` (`semesterID` ASC) VISIBLE,
-    CONSTRAINT `studentclassinstance_ibfk_1`
-        FOREIGN KEY (`studentID`)
-            REFERENCES `operanlys`.`studentinfo` (`studentID`)
+    `instance_id`  INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `student_id`   INT UNSIGNED NULL DEFAULT NULL,
+    `professor_id` INT UNSIGNED NULL DEFAULT NULL,
+    `class_id`     INT UNSIGNED NULL DEFAULT NULL,
+    `semester_id`  INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (instance_id),
+    INDEX `student_id_idx` (student_id ASC) VISIBLE,
+    INDEX `professor_id_idx` (professor_id ASC) VISIBLE,
+    INDEX `class_id_idx` (class_id ASC) VISIBLE,
+    INDEX `semester_id_idx` (semester_id ASC) VISIBLE,
+    CONSTRAINT `student_class_instance_student_info_fk`
+        FOREIGN KEY (student_id)
+            REFERENCES operational_analytics.student_info (student_id)
             ON DELETE CASCADE
             ON UPDATE CASCADE,
-    CONSTRAINT `studentclassinstance_ibfk_2`
-        FOREIGN KEY (`professorID`)
-            REFERENCES `operanlys`.`professorinfo` (`professorID`)
+    CONSTRAINT `student_class_instance_professor_info_fk`
+        FOREIGN KEY (professor_id)
+            REFERENCES operational_analytics.professor_info (professor_id)
             ON DELETE CASCADE
             ON UPDATE CASCADE,
-    CONSTRAINT `studentclassinstance_ibfk_3`
-        FOREIGN KEY (`classID`)
-            REFERENCES `operanlys`.`classinfo` (`classID`)
+    CONSTRAINT `student_class_instance_class_info_fk`
+        FOREIGN KEY (class_id)
+            REFERENCES operational_analytics.class_info (class_id)
             ON DELETE CASCADE
             ON UPDATE CASCADE,
-    CONSTRAINT `studentclassinstance_ibfk_4`
-        FOREIGN KEY (`semesterID`)
-            REFERENCES `operanlys`.`semesterinfo` (`semesterID`)
+    CONSTRAINT `student_class_instance_semester_info_fk`
+        FOREIGN KEY (semester_id)
+            REFERENCES operational_analytics.semester_info (semester_id)
             ON DELETE CASCADE
             ON UPDATE CASCADE
 )
@@ -154,21 +123,18 @@ CREATE TABLE IF NOT EXISTS `operanlys`.`studentclassinstance`
     DEFAULT CHARACTER SET = utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `operanlys`.`uploadlogs`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `operanlys`.`uploadlogs`
+CREATE TABLE IF NOT EXISTS operational_analytics.upload_logs
 (
-    `logID`             INT             NOT NULL,
-    `uploadLogsID`      INT             NOT NULL,
+    `log_id`            INT             NOT NULL,
+    `upload_logs_id`    INT             NOT NULL,
     `filename`          VARCHAR(255)    NULL DEFAULT NULL,
     `filepath`          LONGTEXT        NULL DEFAULT NULL,
-    `successfullUpload` ENUM ('Y', 'N') NULL DEFAULT NULL,
-    PRIMARY KEY (`uploadLogsID`, `logID`),
-    INDEX `fk_uploadLogs_logs1_idx` (`logID` ASC) VISIBLE,
-    CONSTRAINT `fk_uploadLogs_logs1`
-        FOREIGN KEY (`logID`)
-            REFERENCES `operanlys`.`logs` (`logID`)
+    `successful_upload` ENUM ('Y', 'N') NULL DEFAULT NULL,
+    PRIMARY KEY (upload_logs_id, log_id),
+    INDEX `log_id_idx` (log_id ASC) VISIBLE,
+    CONSTRAINT `upload_logs_logs_fk`
+        FOREIGN KEY (log_id)
+            REFERENCES operational_analytics.`logs` (log_id)
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb3;
